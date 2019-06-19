d8691c8da8d0904d447515cc0e043abbf785728c
FernandezJulianS <51395752+FernandezJulianS@users.noreply.github.com>
2019-06-18 23:37:44 -0500

    Add files via upload

---
+++ CalculadoraResiduos.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+	int y,j;
+	float x;
+	cout<<"Digite el dividendo: "<<endl;
+	cin>>y;
+	cout <<"Digite el divisor: "<<endl;
+	cin>>j;
+	x = y%j;
+	cout<<"El residuo es: "<<x<<endl;
+	return 0;
+}
+
+ 
